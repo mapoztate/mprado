c5d15b3061d93f7242d1cc4b34015d5fd25a562e
matt <matthew.f.prado@valkyriepcs.com>
2021-05-13 00:48:26 -0700

    changing tags background

--- src/components/tags.js
+++ src/components/tags.js
@@ -35,7 +35,7 @@ const Tag = styled.span`
   & a {
     position: relative;
     z-index: 2;
-    background-color: rgba(255, 255, 255, 0.7);
+    background-color: rgba(255, 255, 255, 0.3);
     text-decoration: none;
     color: inherit;
     padding: 0.2rem 0.6rem;
