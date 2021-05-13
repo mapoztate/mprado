a70f2a98f49f9b1f117f4f5046f35563bae5dd3d
matt <matthew.f.prado@valkyriepcs.com>
2021-05-12 18:53:55 -0700

    change background color

--- src/components/global-styles.js
+++ src/components/global-styles.js
@@ -106,7 +106,7 @@ body {
   color: #37292C;
   background-attachment: fixed;
   background-color: #d9e4f5;
-  background-image: linear-gradient(315deg, #d9e4f5 0%, #f5e3e6 74%);
+  background-image: #23272A;
 
 }
 
