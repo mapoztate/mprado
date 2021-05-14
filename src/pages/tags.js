13923b75959546229f89ff503cb2dd8a7195bd50
matt <matthew.f.prado@gmail.com>
2021-05-13 18:20:28 -0700

    TRYING AGAIN

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Link, graphql } from 'gatsby';
-import '../../styles.css';
+import '../../styles';
 
 const toKebabCase = (str) => {
   return str
