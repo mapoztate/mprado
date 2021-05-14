01241ec5ed6c2c37f70343ac79905dc16ddc6b58
matt <matthew.f.prado@gmail.com>
2021-05-13 18:14:32 -0700

    fixed path

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Link, graphql } from 'gatsby';
-import '../src/styles.css';
+import '../../styles.css';
 
 const toKebabCase = (str) => {
   return str
