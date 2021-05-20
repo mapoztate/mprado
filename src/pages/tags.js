d15a0e3d39a0be46df019e3991ce5acde066411c
matt <matthew.f.prado@valkyriepcs.com>
2021-05-20 13:23:43 -0700

    resetting to initial commit

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -2,17 +2,11 @@ import React from 'react';
 import Layout from '../components/layout';
 import { Link, graphql } from 'gatsby';
 
-
 const toKebabCase = (str) => {
   return str
     .match(/[A-Z]{2,}(?=[A-Z][a-z]+[0-9]*|\b)|[A-Z]?[a-z]+[0-9]*|[A-Z]|[0-9]+/g)
     .map((x) => x.toLowerCase())
     .join('-');
-};
-
-
-const linkStyle = {
-  color: 'white'
 };
 
 const Tags = ({ data }) => {
@@ -22,7 +16,7 @@ const Tags = ({ data }) => {
     <Layout title="All Tags">
       <h1>All Tags</h1>
 
-      <ul  style={linkStyle}>
+      <ul>
         {tags.map((tag) => (
           <li key={tag.fieldValue}>
             <Link to={`/tags/${toKebabCase(tag.fieldValue)}/`}>
