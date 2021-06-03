fa4d38b880a7c9b0acfd6a0ea2c593ad2fe4c8de
matt <matthew.f.prado@valkyriepcs.com>
2021-06-02 22:05:59 -0700

    im a frontend dev now

--- src/templates/contact-template.js
+++ src/templates/contact-template.js
@@ -134,9 +134,12 @@ const ContactWrapper = styled.div`
 
 const ContactCopy = styled.div`
   max-width: 45ch;
-
+  
+  & a {
+  color: white;  
+  }
+  
   & p {
-  color: white;    
   font-size: var(--size-400);
   }
 `;
