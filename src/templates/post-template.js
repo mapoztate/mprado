e5c7cb9ad326c3e77c4ba1b658cc0c4a0a1df730
matt <matthew.f.prado@valkyriepcs.com>
2021-06-03 01:20:39 -0700

    i knew it wasnt gonna work

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useRef } from "react"
 import { Link, graphql } from 'gatsby';
 import Layout from '../components/layout';
 import styled from 'styled-components';
