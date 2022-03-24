519eccd6456bc502b8fca8a99be6cf47e69623fc
matt <matthew.f.prado@valkyriepcs.com>
2022-03-23 21:26:22 -0700

    checking if table works

--- src/templates/page-template.js
+++ src/templates/page-template.js
@@ -10,16 +10,27 @@ const PageTemplate = ({ data }) => {
       title={frontmatter.title}
       description={frontmatter.description || excerpt}
     >
+    <PostWrapper>
       <article>
         <h1>{frontmatter.title}</h1>
 
         <section dangerouslySetInnerHTML={{ __html: html }} />
       </article>
+      </PostWrapper>
     </Layout>
   );
 };
 
 export default PageTemplate;
+
+const PostWrapper = styled.div`
+  @media screen and (max-width: 1000px) {
+    & td, h1 {
+      display: block;
+      text-align: center;
+      width: 100%;
+    }
+`;
 
 export const pageQuery = graphql`
   query PageBySlug($slug: String!) {
@@ -33,3 +44,5 @@ export const pageQuery = graphql`
     }
   }
 `;
+
+
--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -80,13 +80,6 @@ const PostWrapper = styled.div`
   margin-right: auto;
   max-width: 70ch;
   word-wrap: break-word;
-
-  @media screen and (max-width: 1000px) {
-    & td, h1 {
-      display: block;
-      text-align: center;
-      width: 100%;
-    }
 `;
 
 const PostTitle = styled.h1`
